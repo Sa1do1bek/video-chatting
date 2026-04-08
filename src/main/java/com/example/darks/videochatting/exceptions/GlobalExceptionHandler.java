ece25335921efdf9656e8e3a862f31f77b736ca1
Sardor <sardo1be1@gmail.com>
2026-04-08 16:55:47 +0500

    global exception handler has been fixed

--- src/main/java/com/example/darks/videochatting/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/darks/videochatting/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.example.darks.videochatting.exceptions;
 
-import com.example.backend.enums.ErrorType;
-import com.example.backend.responses.ApiResponse;
-import com.example.backend.services.error.ErrorLoggingService;
+import com.example.darks.videochatting.dtos.base.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,88 +8,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-    private final ErrorLoggingService errorLoggingService;
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ApiResponse> handleBadRequest(
-            ResourceNotFoundException ex,
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleBadUserRequest(
+            UserNotFoundException ex,
             HttpServletRequest request
     ) {
-
-        errorLoggingService.logError(
-                request.getRequestURI(),
-                request.getMethod(),
-                400,
-                ex.getMessage(),
-                ErrorType.BAD_REQUEST,
-                ""
-        );
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ApiResponse(ex.getMessage(), null));
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse> handleNotFound(
-            ResourceNotFoundException ex,
+    @ExceptionHandler(MeetingNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleBadMeetingRequest(
+            UserNotFoundException ex,
             HttpServletRequest request
     ) {
-
-        errorLoggingService.logError(
-                request.getRequestURI(),
-                request.getMethod(),
-                404,
-                ex.getMessage(),
-                ErrorType.NOT_FOUND,
-                ""
-        );
-
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(new ApiResponse(ex.getMessage(), null));
-    }
-
-    @ExceptionHandler(AsyncRequestTimeoutException.class)
-    public ResponseEntity<ApiResponse> handleTimeout(
-            AsyncRequestTimeoutException ex,
-            HttpServletRequest request
-    ) {
-        errorLoggingService.logError(
-                request.getRequestURI(),
-                request.getMethod(),
-                504,
-                ex.getMessage(),
-                ErrorType.TIMEOUT,
-                ex.getMessage()
-        );
-
-        return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
-                .body(new ApiResponse(ex.getMessage(), null));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse> handleException(
-            Exception ex,
-            HttpServletRequest request
-    ) {
-
-        errorLoggingService.logError(
-                request.getRequestURI(),
-                request.getMethod(),
-                500,
-                ex.getMessage(),
-                ErrorType.INTERNAL_SERVER_ERROR,
-                Arrays.toString(ex.getStackTrace())
-        );
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .status(HttpStatus.BAD_REQUEST)
                 .body(new ApiResponse(ex.getMessage(), null));
     }
 }
